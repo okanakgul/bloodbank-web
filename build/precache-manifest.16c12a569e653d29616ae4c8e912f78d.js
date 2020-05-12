b91891582c8e65ee128881323070614f779e3b84
Okan Akgül <kakgul16@ku.edu.tr>
2020-05-12 16:35:26 +0300

    built

---
+++ build/precache-manifest.16c12a569e653d29616ae4c8e912f78d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d81ad32a4bed2fcc75543e9820e81fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d921697a658aaa12bfdd",
+    "url": "/static/css/main.3084f400.chunk.css"
+  },
+  {
+    "revision": "79310d550237ede3919f",
+    "url": "/static/js/2.adb08a2f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.adb08a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d921697a658aaa12bfdd",
+    "url": "/static/js/main.bf304f9d.chunk.js"
+  },
+  {
+    "revision": "9e64234d4f74bd4b8053",
+    "url": "/static/js/runtime-main.591c73fc.js"
+  },
+  {
+    "revision": "72284d10a94ea33883cb25848a9e34bb",
+    "url": "/static/media/blood-bag.72284d10.jpg"
+  },
+  {
+    "revision": "9532f891f96b79ca1bbfa6d9665df645",
+    "url": "/static/media/logo.9532f891.jpg"
+  }
+]);
